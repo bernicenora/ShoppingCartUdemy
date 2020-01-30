a056c8fa5b111b4f2f13965d753508b935909e0b
BE <bernicewebdesigner@gmail.com>
2020-01-30 13:51:12 -0700

    Added code to add the selected course into a table row in the cart

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 // Variables
-const courses = document.querySelector("#courses-list");
+const courses = document.querySelector("#courses-list"),
+    shoppingCartContent = document.querySelector('#cart-content tbody');
 
 
 // Listeners
@@ -49,6 +50,18 @@ function addIntoCart(course){
 
     // Build the template
     row.innerHTML = `
-    
-    `
+    <tr>
+        <td>
+            <img src="${course.image}" width="100px">
+        </td>
+        <td>${course.title}</td>
+        <td>${course.price}</td>
+        <td>
+            <a href="#" class="remove" data-id="${course.id}">X</a>
+        </td>
+    </tr>
+    `;
+
+    //Add into the the shopping cart
+    shoppingCartContent.appendChild(row);
 }
